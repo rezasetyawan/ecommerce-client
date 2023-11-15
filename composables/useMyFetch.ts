58ee76ecf64d43a1bfb0c36d10815cdbe7857dcf
rezasetyawan <setyawanreza960@gmail.com>
2023-11-15 22:08:45 +0700

    hotfix: cors error

--- composables/useMyFetch.ts
+++ composables/useMyFetch.ts
@@ -4,5 +4,8 @@ export const useMyFetch = (endpoint: string, options = {}) => {
         ...options,
         baseURL: config.public.API_BASE_URL,
     };
-    return useFetch(endpoint, newOptions);
+    return useFetch(endpoint, {
+        mode: 'no-cors',
+        ...newOptions
+    });
 };
